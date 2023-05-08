ee234a9932230c82b7f957097d87797c19ca7f33
ManuelPerson <127495647+ManuelPerson@users.noreply.github.com>
2023-05-08 12:20:36 +0200

    Update and rename PythonprogrammFürIT-GrundschutzISMSPrototype3.py to PythonProgrammFür_IT-GrundschutzISMSPrototype3.py

--- PythonProgrammFür_IT-GrundschutzISMSPrototype3.py
+++ PythonProgrammFür_IT-GrundschutzISMSPrototype3.py
@@ -46,7 +46,7 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt X
+# Schritt 4
 print("Die Erstellung der Leitlinie zur Informationssicherheit, liegt im Verantwortungsbereich der Geschäftsleitung.")
 print("Hinweis: Die Leitlinie beschreibt, welche Sicherheitsziele und welches Sicherheitsniveau die Institution anstrebt, "
       "\nwas die Motivation hierfür ist und mit welchen Maßnahmen und mit welchen Strukturen dies erreicht werden soll. "
@@ -54,7 +54,7 @@ print("Hinweis: Die Leitlinie beschreibt, welche Sicherheitsziele und welches Si
 print("Bitte bestätigen Sie, dass eine Leitlinie zur Informationssicherheit erstellt wurde, und die Leitline allen Mitarbeitern bekannt gegeben wurde .")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt X abgeschlossen.")
+    print("Schritt 4 abgeschlossen.")
 else:
     print("Allein die Leitungsebene darf eine geeignete Vorgehensweise bezüglich der Absicherungsarten wählen "
           "\nund eine Leitlinie zur Informationssicherheit erstellen. Ebenso müssen durch die Geschäftsleitung Rollen und Aufgaben benannt und verteilt werden.")
@@ -63,14 +63,14 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 4
+# Schritt 5
 print("Aufbau einer geeigneten Organisationsstruktur für das Informationssicherheitsmanagement.")
 print("Hinweis: Für das Informationssicherheitsmanagement muss eine, für Größe und Art der Institution geeignete Organisationsstruktur aufgebaut werden.")
 print("Bitte bestätigen Sie, dass eine geeignete Organisationsstruktur (ggf. ISMS-Team) für das Informationssicherheitsmanagement durch die, "
       "\nvon der Geschäftsleitung benannten Personen aufgebaut wurde.")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 4 abgeschlossen.")
+    print("Schritt 5 abgeschlossen.")
 else:
     print("Bitte bauen Sie gemeinsam mit der Leitungsebene eine geeignete Organisationsstruktur für das Informationssicherheitsmanagement auf.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -78,7 +78,7 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 5
+# Schritt 6
 print("Erstellung einer Sicherheitskonzeption. Die Umsetzung der Sicherheitskonzeption wird durch die Geschäftleitung beauftragt.")
 print("Hinweis: Nachdem ein Informationssicherheitsprozess initiiert wurde und die Sicherheitsleitlinie "
       "\nund Informationssicherheitsorganisation definiert wurden, wird die Sicherheitskonzeption für die Institution erstellt. "
@@ -87,7 +87,7 @@ print("Hinweis: Nachdem ein Informationssicherheitsprozess initiiert wurde und d
 print("Bitte bestätigen Sie, dass eine Sicherheitskonzeption erstellt und die Umgesetzung von der Geschäftsleitung in Auftrag gegeben wurde.")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 5 abgeschlossen.")
+    print("Schritt 6 abgeschlossen.")
 else:
     print("Bitte achten Sie darauf, das eine durch die Geschäftsleitung abgenommene und unterschiebene Sicherheitskonzeption vorliegt "
           "\nund setzen Sie diese Ihrem Auftrag entsprechend um. Starten Sie nun mit der implementierung des ISMS.")
@@ -96,12 +96,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 6
+# Schritt 7
 print("Definition des Informationsverbunds im Geltungsbereich.")
 print("Wurde der Geltungsbereich der IT-Grundschutzmaßnahmen definiert und festgelegt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 6 abgeschlossen.")
+    print("Schritt 7 abgeschlossen.")
 else:
     print("Bitte achten Sie zunächst darauf, dass Geltungsbereich der IT-Grundschutzmaßnahmen klar definiert wurde.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -109,12 +109,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 7
+# Schritt 8
 print("Durchführung einer Strukturanalyse und Erstellung des Netzwerkplans.")
 print("Haben Sie eine übersichtliche Struktur des Informationsverbunds erstellt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 7 abgeschlossen.")
+    print("Schritt 8 abgeschlossen.")
 else:
     print("Bitte führen Sie die Strukturanalyse und die Erstellung eines Netzwerkplans durch.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -122,12 +122,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 8
+# Schritt 9
 print("Durchführung einer Schutzbedarfsfeststellung.")
 print("Haben Sie den Schutzbedarf für die einzelnen Komponenten innerhalb des Informationsverbunds ermittelt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 8 abgeschlossen.")
+    print("Schritt 9 abgeschlossen.")
 else:
     print("Bitte führen Sie eine Schutzbedarfsfeststellung durch.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -135,12 +135,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 9
+# Schritt 10
 print("Modellierung des Informationsverbunds.")
 print("Haben Sie ein Modell des Informationsverbunds erstellt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 9 abgeschlossen.")
+    print("Schritt 10 abgeschlossen.")
 else:
     print("Bitte erstellen Sie zunächst ein Modell des Informationsverbunds. "
           "\nZur Modellierung nach IT-Grundschutz: "
@@ -152,14 +152,14 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 10
+# Schritt 11
 print("Durchführung des IT-Grundschutz-Checks (Soll-Ist-Vergleich).")
 print("Hinweis: Die Ergebnisse des IT-Grundschutz-Checks sollten so dokumentiert werden, "
       "\ndass sie für alle Beteiligten nachvollziehbar sind und als Grundlage für die Umsetzungsplanung der defizitären Anforderungen und Maßnahmen genutzt werden können.")
 print("Haben Sie den IT-Grundschutz-Check durchgeführt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 10 abgeschlossen.")
+    print("Schritt 11 abgeschlossen.")
 else:
     print("Bitte führen Sie zunächst den IT-Grundschutz-Check durch (Durchführung des Soll-Ist-Vergleichs). "
           "\nBei der Erhebung des erreichten Sicherheitsstatus werden die Sicherheitsanforderungen des jeweiligen Bausteins der Reihe nach durchgearbeitet")
@@ -168,12 +168,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 11
+# Schritt 12
 print("Die Risikoanalyse.")
 print("Haben Sie eine Risikoanalyse durchgeführt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 11 abgeschlossen.")
+    print("Schritt 12 abgeschlossen.")
 else:
     print("Bitte führen Sie zunächst eine Risikoanalyse durch.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut.'
@@ -181,12 +181,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 12
+# Schritt 13
 print("Konsolidierung der Maßnahmen.")
 print("Haben Sie nach der Risikoanalyse eine Liste aller erforderlichen Schutzmaßnahmen erstellt?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 12 abgeschlossen.")
+    print("Schritt 13 abgeschlossen.")
 else:
     print("Bitte erstellen Sie zunächst eine Liste aller erforderlichen Maßnahmen.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -194,12 +194,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 13
+# Schritt 14
 print("Umsetzung aller Maßnahmen der in Schritt 12 aufgelisteten Maßnahmen.")
 print("Haben Sie alle Maßnahmen Ihrer Liste umgesetzt? (begründen Sie ggf. die Nichtumsetzung in Ihrer Dokumentation.)")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 13 abgeschlossen.")
+    print("Schritt 14 abgeschlossen.")
 else:
     print("Bitte setzen Sie zunächst alle zutreffenden Maßnahmen um, oder begründen ggf. sie die Nichtumsetzung.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -207,12 +207,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 14
+# Schritt 15
 print("Aufrechterhaltung und kontinuierliche Verbesserung der Informationssicherheit.")
 print("Haben Sie sichergestellt, dass die Informationssicherheit kontinuierlich aufrechterhalten und verbessert wird?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 14 abgeschlossen.")
+    print("Schritt 15 abgeschlossen.")
 else:
     print("Bitte stellen Sie zunächst sicher, dass die Informationssicherheit kontinuierlich aufrechterhalten und verbessert wird.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -220,12 +220,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 15
+# Schritt 16
 print("Fortentwicklung des ISMS.")
 print("Entwickeln Sie das ISMS kontinuierlich weiter?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 15 abgeschlossen.")
+    print("Schritt 16 abgeschlossen.")
 else:
     print("Bitte entwickeln Sie das ISMS kontinuierlich weiter.")
     print('Bitte führen Sie zunächst die fehlenden Schritte aus und starten Sie das Programm erneut. '
@@ -233,12 +233,12 @@ else:
     input("")
     sys.exit()
 print("")
-# Schritt 16
+# Schritt 17
 print("Erweiterung der gewählten Vorgehensweise.")
 print("Erweitern Sie die gewählte Vorgehensweise (im Bezug auf Kern- und Standard-Absicherung) bei Bedarf?")
 confirmation = input("Ja/Nein: ")
 if confirmation.lower() == "ja":
-    print("Schritt 16 abgeschlossen.")
+    print("Schritt 17 abgeschlossen.")
     print("")
     print("Herzlichen Glückwunsch! Ihr ISMS ist nun implementiert. Ihr ISMS bedarf auch weiterhin kontinuierliche Verbesserung und Pflege.")
     print("Bleiben Sie also am Ball! Informationssicherheit ist kein einmalig herzustellender Zustand.")
